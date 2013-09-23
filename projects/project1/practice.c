778fe89bb68f0c99d57eee64f8c104575e5b443c
darylx <xayav001@umn.edu>
2013-09-23 07:19:43 -0500

    added a project 1 folder and project

---
+++ projects/project1/practice.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]){
+    
+	int X = open("./output_file.txt", O_WRONLY);
+	
+    pid_t pid;
+    
+	int status = 0;
+	
+    int count = 0;
+    int parentCount = 0;
+    
+    printf("before the fork\n");
+    
+    pid = fork();
+	
+    if(pid == 0){
+        printf("this is the child process\n");
+        
+		printf("child pid: %d\n",pid);
+		
+		execl("/bin/ls", "ls -l", (char*)0);
+		
+		while (count < 10) {
+			count++;
+			sleep(1);
+			printf("Counting: %d\n", count);
+		}
+		
+		
+		
+		//printf("\nswag\n");
+		
+		//exit(status);
+    }else if(pid > 0 ){
+		printf("parent pid: %d\n",pid);
+		
+		//wait(&status);
+        printf("the parent process\n");
+        while(parentCount < 10){
+			printf("Parent proccess: %d\n", parentCount);  
+			sleep(1);
+			parentCount++;
+        } 
+		
+    }
+    
+	close(X);
+    
+    return 0;   
+}
